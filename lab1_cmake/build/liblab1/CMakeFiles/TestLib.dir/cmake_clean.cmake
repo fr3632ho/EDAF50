7d42e7e389f7f6a66503585253ecee209136b0a4
fr3632ho-s <fr3632ho-s@student.lu.se>
2022-01-21 12:15:43 +0100

    done lab 1

---
+++ lab1_cmake/build/liblab1/CMakeFiles/TestLib.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TestLib.dir/coding.cc.o"
+  "CMakeFiles/TestLib.dir/list.cc.o"
+  "libTestLib.a"
+  "libTestLib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TestLib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
